3af8128cfd2a8357462a34f8e2b0c21adbb4056d
michal-wolski <michal.wolski130@gmail.com>
2022-04-06 22:14:44 +0200

    notiflix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/main.scss';
 import axios, { Axios } from 'axios';
+import Notiflix from 'notiflix';
 
 //DOM import
 const input = document.querySelector("input")
@@ -7,22 +8,23 @@ const btn = document.querySelector('button')
 
 async function getFoto(words) {
     const apiKey = '26531596-66f70a56847dae6fbc6ddebb0';
-    const url = `https://pixabay.com/api/?key=${apiKey}&q=${words}&image_type=photo`
-    // const searchWord = toString(words)
-    // console.log(searchWord);
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${words}&image_type=photo`;
     try {
       const response = await axios.get(url);
-      const data = response.data;
-      const hits = response.data.hits;
+      const data = await response.data;
+      const hits = await response.data.hits;
       console.log(hits);
-      console.log(data);
     } catch (error) {
+      if (hits === []) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+      }
       console.error(error);
     }
   }
 
-btn.addEventListener('submit', ()=> {
+btn.addEventListener('click', ()=> {
     const result = input.value
     getFoto(result)
 });
 
+getFoto('aaaaaaaaaaaaa')
