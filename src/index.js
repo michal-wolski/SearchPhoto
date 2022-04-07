5617b4d4306b972b5889c545c0108815b597f34a
michal-wolski <michal.wolski130@gmail.com>
2022-04-07 23:05:16 +0200

    gallery create

--- src/index.js
+++ src/index.js
@@ -6,25 +6,63 @@ import Notiflix from 'notiflix';
 const input = document.querySelector("input")
 const btn = document.querySelector('button')
 
-async function getFoto(words) {
-    const apiKey = '26531596-66f70a56847dae6fbc6ddebb0';
-    const url = `https://pixabay.com/api/?key=${apiKey}&q=${words}&image_type=photo`;
-    try {
-      const response = await axios.get(url);
-      const data = await response.data;
-      const hits = await response.data.hits;
-      console.log(hits);
-    } catch (error) {
-      if (hits === []) {
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-      }
-      console.error(error);
-    }
+//Base URL
+const apiKey = '26531596-66f70a56847dae6fbc6ddebb0';
+const url = `https://pixabay.com/api/?key=${apiKey}&q=car&image_type=photo`;
+//Fetch image from http
+async function getFoto() {
+  try {
+    const response = await axios.get(url);
+    const fotoArray = response.data.hits;
+    return fotoArray;
+  } catch (error) {
+    console.log(error);
   }
-
-btn.addEventListener('click', ()=> {
-    const result = input.value
-    getFoto(result)
-});
-
-getFoto('aaaaaaaaaaaaa')
+};
+const x = getFoto();
+//Create gallery funcion
+function createGallery(array) {
+  const gallery = array.map({
+    const test =`<div class="photo-card">
+      <img class="gallery__image" src="${array.webformatURL} "alt="${array.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>${array.likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>${array.views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>${array.comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>${array.downloads}
+        </p>
+      </div>
+    </div>`
+    return test
+    console.log(gallery);
+  });
+createGallery(x);
+//         `<div class="photo-card">
+//           <img class="gallery__image" src="${backendObj.webformatURL}" alt="${backendObj.tags}" loading="lazy" />
+//           <div class="info">
+//             <p class="info-item">
+//               <b>Likes</b>${backendObj.likes}
+//             </p>
+//             <p class="info-item">
+//               <b>Views</b>${backendObj.views}
+//             </p>
+//             <p class="info-item">
+//               <b>Comments</b>${backendObj.comments}
+//             </p>
+//             <p class="info-item">
+//               <b>Downloads</b>${backendObj.downloads}
+//             </p>
+//           </div>
+//         </div>`,
+//     )
+//     .join('');
+//   console.log(markup);
+// }
+// createGalleryTags(x)
